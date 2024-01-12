dce9959ce653d4b928ea1e16f10b414a89ef835c
Suraj Soren <surajsoren456@gmail.com>
2024-01-13 01:55:39 +0530

    fix: Corrected the error classes

--- src/controllers/user/error.ts
+++ src/controllers/user/error.ts
@@ -3,28 +3,35 @@ import { Prisma } from "@prisma/client";
 import { z } from "zod";
 
 export class UserAlreadyExistsError extends Error {
-  constructor(email: string){
+  constructor(email: string) {
     super(`User with email ${email} already exists`);
-    this.name = 'UserAlreadyExistsError';
-    }
+    this.name = "UserAlreadyExistsError";
+    Object.setPrototypeOf(this, UserAlreadyExistsError.prototype);
+  }
 }
 
 export class UserNotExistError extends Error {
-  constructor(email: string){
+  constructor(email: string) {
     super(`User with email ${email} does not exists`);
-    this.name = 'UserNotExist';
-    }
+    this.name = "UserNotExist";
+    Object.setPrototypeOf(this, UserNotExistError.prototype);
+  }
 }
 
 export class UserPasswordIncorrectError extends Error {
-  constructor(email: string){
+  constructor(email: string) {
     super(`User with email ${email} password is incorrect`);
-    this.name = 'UserPasswordIncorrect';
-    }
+    this.name = "UserPasswordIncorrect";
+    Object.setPrototypeOf(this, UserPasswordIncorrectError.prototype);
+  }
 }
 
 export function handleUserSignupError(res: express.Response, error: any) {
-  if (error instanceof z.ZodError || error instanceof Prisma.PrismaClientKnownRequestError) {
+  if (
+    error instanceof z.ZodError ||
+    error instanceof Prisma.PrismaClientKnownRequestError ||
+    error instanceof UserAlreadyExistsError
+  ) {
     res.status(400).json({
       status: "error",
       error: error.message,
@@ -38,17 +45,17 @@ export function handleUserSignupError(res: express.Response, error: any) {
 }
 
 export function handleUserLoginError(res: express.Response, error: any) {
-  if (error instanceof z.ZodError || error instanceof Prisma.PrismaClientKnownRequestError) {
+  if (
+    error instanceof z.ZodError ||
+    error instanceof Prisma.PrismaClientKnownRequestError ||
+    error instanceof UserNotExistError ||
+    error instanceof UserPasswordIncorrectError
+  ) {
     res.status(400).json({
       status: "error",
       error: error.message,
     });
-  } else if(error instanceof UserNotExistError || error instanceof UserPasswordIncorrectError){
-    res.status(400).json({
-      status: "error",
-      error: error.message,
-    });
-  }else{
+  } else {
     res.status(500).json({
       status: "error",
       error: "Something went wrong",
