78c813e6dee7b902b2b87b0992119dfa7da63609
Anuj Sharma <anuj992393@gmail.com>
2024-03-17 09:33:31 +0530

    removed cors to check

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@ import express from "express";
 import apirouter from "./src/router";
 const app = express();
 
-import cors from "cors";
+// import cors from "cors";
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 
